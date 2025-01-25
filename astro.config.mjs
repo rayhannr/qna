d2f698f5f49c108be02409a15e5290f0a2b547e4
rayhannr <rayhannr07@gmail.com>
2025-01-25 16:33:04 +0700

    feat: init shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
-import react from '@astrojs/react';
-
-import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   server: { port: 3000 },
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind({ applyBaseStyles: false })]
 })
